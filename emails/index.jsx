16b8e25502bb13ff5c00b35424c214b76611d1ac
Aman Singh <88573178+aman-q@users.noreply.github.com>
2024-04-13 03:28:23 +0530

    Final code

--- emails/index.jsx
+++ emails/index.jsx
@@ -1,40 +1,54 @@
 import {
-    Body,
-    Button,
-    Container,
-    Head,
-    Hr,
-    Html,
-    Img,
-    Preview,
-    Section,
-    Text,
-  } from "@react-email/components";
-  import * as React from "react";
-  
- 
-  
-  const baseUrl = process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : "http://localhost:3000";
-  
-  export const EmailTemplate = ({
-    UserName,
-    Email,
-    Time,
-    Date,
-    doctor,
-    Note
-  }) => (
+  Body,
+  Button,
+  Container,
+  Head,
+  Hr,
+  Html,
+  Img,
+  Preview,
+  Section,
+  Text,
+} from "@react-email/components";
+import * as React from "react";
+
+const baseUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "http://localhost:3000";
+
+export const EmailTemplate = ({
+  UserName,
+  Email,
+  Time,
+  Date,
+  doctor,
+  Note
+}) => {
+
+  function generateUniqueId() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const length = 10;
+    let uniqueId = '';
+
+    for (let i = 0; i < length; i++) {
+      uniqueId += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+
+    return uniqueId;
+  }
+
+  const confirmationCode = generateUniqueId();
+
+  return (
     <Html>
       <Head />
       <Preview>
-        The sales intelligence platform that helps you uncover qualified leads.
+        Your doctor appointment is confirmed!
       </Preview>
       <Body style={main}>
         <Container style={container}>
           <Img
-            src={`${baseUrl}/logo.svg`}
+            src="logo.svg"
             width="170"
             height="50"
             alt="Koala"
@@ -42,13 +56,12 @@ import {
           />
           <Text style={paragraph}>Hi {UserName},</Text>
           <Text style={paragraph}>
-           Your Appointment with {doctor} has booked on {Date} at {Time}
+            Your appointment is now confirmed. Head over to your booking section for the full scoop. We're eagerly anticipating your visit, ready to provide top-notch care tailored just for you. See you soon!
+            <br />
+            <span style={confirmationCodeStyle}>Confirmation Code: {confirmationCode}</span>
+            <br />
+            Present this code at the hospital to confirm your appointment.
           </Text>
-          {/* <Section style={btnContainer}>
-            <Button style={button} href="https://getkoala.com">
-              Get started
-            </Button>
-          </Section> */}
           <Text style={paragraph}>
             Best,
             <br />
@@ -56,59 +69,63 @@ import {
           </Text>
           <Hr style={hr} />
           <Text style={footer}>
-            Developed by TubeGuruji!
+            Developed by Aman Singh
           </Text>
         </Container>
       </Body>
     </Html>
   );
-  
+}
 
-  
-  export default EmailTemplate;
-  
-  const main = {
-    backgroundColor: "#ffffff",
-    fontFamily:
-      '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif',
-  };
-  
-  const container = {
-    margin: "0 auto",
-    padding: "20px 0 48px",
-  };
-  
-  const logo = {
-    margin: "0 auto",
-  };
-  
-  const paragraph = {
-    fontSize: "16px",
-    lineHeight: "26px",
-  };
-  
-  const btnContainer = {
-    textAlign: "center",
-  };
-  
-  const button = {
-    backgroundColor: "#5F51E8",
-    borderRadius: "3px",
-    color: "#fff",
-    fontSize: "16px",
-    textDecoration: "none",
-    textAlign: "center",
-    display: "block",
-    padding: "12px",
-  };
-  
-  const hr = {
-    borderColor: "#cccccc",
-    margin: "20px 0",
-  };
-  
-  const footer = {
-    color: "#8898aa",
-    fontSize: "12px",
-  };
-  
+export default EmailTemplate;
+
+const main = {
+  backgroundColor: "#ffffff",
+  fontFamily:
+    '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif',
+};
+
+const container = {
+  margin: "0 auto",
+  padding: "20px 0 48px",
+};
+
+const logo = {
+  margin: "0 auto",
+};
+
+const paragraph = {
+  fontSize: "18px",
+  lineHeight: "26px",
+};
+
+const confirmationCodeStyle = {
+  fontSize: "18px",
+  fontWeight: "bold",
+  color: "#5F51E8",
+};
+
+const btnContainer = {
+  textAlign: "center",
+};
+
+const button = {
+  backgroundColor: "#5F51E8",
+  borderRadius: "3px",
+  color: "#fff",
+  fontSize: "16px",
+  textDecoration: "none",
+  textAlign: "center",
+  display: "block",
+  padding: "12px",
+};
+
+const hr = {
+  borderColor: "#cccccc",
+  margin: "20px 0",
+};
+
+const footer = {
+  color: "#8898aa",
+  fontSize: "12px",
+};
